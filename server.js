5dcad9a8d60c94278f4bca25740eb57a31a9f579
alexxchavez <70489111+alexxchavez@users.noreply.github.com>
2021-04-09 12:39:28 -0500

    Express Router Part I

--- server.js
+++ server.js
@@ -1,47 +1,63 @@
-const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const path = require('path');
-const fs = require('fs');
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
 
-const server = http.createServer((req, res) => {
-    console.log(`Request for ${req.url} by method ${req.method}`);
-
-    if (req.method === 'GET') {
-        let fileUrl = req.url;
-        if (fileUrl === '/') {
-            fileUrl = '/index.html';
-        }
-
-        const filePath = path.resolve('./public' + fileUrl);
-        const fileExt = path.extname(filePath);
-        if (fileExt === '.html') {
-            fs.access(filePath, err => {
-                if (err) {
-                    res.statusCode = 404;
-                    res.setHeader('Content-Type', 'text/html');
-                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
-                    return;
-                }
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-
-                fs.createReadStream(filePath).pipe(res);
-            });
-        } else {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
-        }
-    } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
-    }
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
 });
 
-server.listen(port, hostname, () => {
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteID} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
