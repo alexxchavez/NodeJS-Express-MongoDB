fd992589ea62049434b4cfdd82fdcf84174875fb
alexxchavez <70489111+alexxchavez@users.noreply.github.com>
2021-04-21 14:42:39 -0500

    Mongoose Population

--- campsiteRouter.js
+++ campsiteRouter.js
@@ -9,6 +9,7 @@ campsiteRouter.route('/')
 //express routing methods: .get(), .post(), .put(), .delete()
 .get((req, res, next) => {
     Campsite.find()
+    .populate('comments.author')
     .then(campsites => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -46,6 +47,7 @@ campsiteRouter.route('/')
 campsiteRouter.route('/:campsiteId')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -82,6 +84,7 @@ campsiteRouter.route('/:campsiteId/comments')
 .get((req, res, next) => {
     //findById finds single campsite id
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite) {
             res.statusCode = 200;
@@ -99,6 +102,7 @@ campsiteRouter.route('/:campsiteId/comments')
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite) {
+            req.body.author = req.user._id;
             campsite.comments.push(req.body);
             campsite.save()
             .then(campsite => {
@@ -146,6 +150,7 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
 //express routing methods: .get(), .post(), .put(), .delete()
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
             res.statusCode = 200;
